6004a49571ba441fa18e0ced4a56a3933a051e75
CDE20IJ012-Ruksar <80447475+CDE20IJ012-Ruksar@users.noreply.github.com>
2021-04-12 12:53:29 +0530

    Add files via upload

---
+++ BasicSecurity/BasicSecurity/src/main/java/com/cognizant/HelloController.java
@@ -0,0 +1,23 @@
+package com.cognizant;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/")
+	public String hello() {
+		return "Hello";
+	}
+	
+	
+	@GetMapping("/user")
+	public String helloForUser() {
+		return "Hello User";
+	}
+	
+	@GetMapping("/admin")
+	public String helloForAdmin() {
+		return "Hello Admin";
+	}
+}
